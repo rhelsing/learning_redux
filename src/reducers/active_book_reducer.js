6e5f82456447916bf9f72f7d96b9dc0a3e5b60e4
Ryan Helsing <ryanhelsing@gmail.com>
2017-07-29 20:18:16 -0400

    final

--- src/reducers/active_book_reducer.js
+++ src/reducers/active_book_reducer.js
@@ -1,10 +1,11 @@
 //state is not application state, only responsible for
 export default function(state = null, action){
 
+  //only handle responses that we should
   switch(action.type) {
   case 'BOOK_SELECTED':
     return action.payload
   }
-  return state
+  return state //return self otherwise
 
 }
